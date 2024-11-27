377015a1f2b780c0edc7f43d4c5a328b681d17f4
jayjay <167156089+jjnootfound@users.noreply.github.com>
2024-11-27 17:27:44 -0600

    fix(claw: Fixed position and added autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HumanPlayerAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HumanPlayerAuto.java
@@ -3,14 +3,40 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.subsystems.Claw;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Slider;
 
 @Autonomous
 public class HumanPlayerAuto extends LinearOpMode {
     private Drivetrain drivetrain;
+    private Slider slider;
+    private Claw claw;
 
     @Override
     public void runOpMode() throws InterruptedException {
         drivetrain = new Drivetrain(hardwareMap);
+        claw = new Claw(hardwareMap);
+
+        waitForStart();
+
+        drivetrain.drive(1, Math.PI/2,0);
+        sleep(500);
+        drivetrain.stop();
+
+        slider.expandSliders();
+        sleep(200);
+        slider.stopSliders();
+
+        slider.contractSliders();
+        sleep(200);
+        slider.contractSliders();
+
+        claw.openClaw();
+        sleep(100);
+
+        drivetrain.drive(1, 11 * Math.PI/2,1);
+        sleep(500);
+        drivetrain.stop();
     }
 }
